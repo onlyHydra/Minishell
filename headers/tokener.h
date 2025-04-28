5e2d5b57a5b07c506eb6936b489e1112fb1713db
Idris A. <idris@Idriss-MacBook-Pro.local>
2025-04-29 01:47:26 +0200

    [NEW HEADER] #60 - new header and changed tokener.h

--- headers/tokener.h
+++ headers/tokener.h
@@ -30,81 +30,16 @@
 # include "../includes/Libft/get_next_line/get_next_line.h"
 # include "../includes/Libft/libft.h"
 # include "token_struct.h"
-/* Enums and Structures */
+# include "token_interface.h"
 
 /* ============================= */
 /*         DECLARATIONS         */
 /* ============================= */
 
-/* --- Main Functions and Wrappers --- */
-t_token			*wrapper_process_string(char *input, char **envp);
+/* --- Main Functions --- */
 t_parsed_data	*tokenize_input(char **argv, char **envp);
 
-// Move the function away with First Occasion
-int				has_env_vars(char *str);
+/* --- Utility Functions --- */
+int				ft_is_whitespace(char c);
 
-// Move into Parsing Header with First Occasion
-int				handle_operator_segment(t_parse_params *params, int i);
-void			process_segment(t_parse_params *params);
-void			handle_segment(t_parse_params *params, int i);
-
-/* --- Parse State Init move to Parser header with First Occasion --- */
-void			init_parse_state(t_parse_state *state, t_token **tokens);
-void			init_parse_params(t_parse_params *params, char *input,
-					t_token **tokens, char **envp);
-
-/* --- Token Processing --- */
-void			process_token(char *input, t_parse_state *state, int end,
-					char **envp);
-
-int				handle_whitespace(char *input, t_parse_state *state,
-					char **envp);
-int				handle_parenthesis_char(char *input, t_parse_state *state,
-					char **envp);
-int				handle_quotes(char *input, t_parse_state *state);
-int				handle_regular_text(char *input, t_parse_state *state,
-					char **envp);
-/* --- Token Type Logic --- */
-
-int				is_builtin_command(char *token);
-
-/* --- Token Operations --- */
-char			*extract_string(char *input, int start, int end);
-int				handle_parsing_ops(char *input, t_parse_state *state,
-					char **envp);
-
-// UTILS TOKEN
-char			*extract_string(char *input, int start, int end);
-int				calculate_total_length(char **argv);
-char			*concatenate_arguments(char **argv, int total_len);
-
-/* --- Command Logic --- */
-void			apply_command_flags(t_token *tokens, int *cmd_flags);
-int				is_command_in_path(char *cmd);
-int				executable(char *token, char **envp);
-
-/* --- String utils --- */
-int				is_quote_closed(char *str, int start, char quote_char);
-int				handle_quotes_tokenize(char *input, int i, int *in_quote,
-					char *quote_char);
-int				handle_braces(char *input, t_parse_state *state);
-int				is_operator(char *input, int i);
-
-int				handle_quoted_string(char *str, int i, t_token_type quote_type,
-					int *error);
-void			free_array(char **dirs);
-void			post_process_command_tokens(t_token *tokens, char **envp);
-// DONT TOUCH
-int				is_string_command(char *string, char **envp);
-
-// MEMORY MANAGMENT
-void			free_single_token(t_token *token);
-void			free_token_struct(t_token *tokens);
-void			free_params_struct(t_parse_params *params);
-void			free_parsed_data(t_parsed_data *parsed_data);
-void			free_state_struct(t_parse_state *state);
-void			free_parsed_data_up_to(t_parsed_data *parsed_data,
-					int up_to_index);
-void			free_parsed_data_on_error(t_parsed_data *parsed_data, int index,
-					int free_token);
 #endif
