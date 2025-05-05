88f14151506edfd9170a7b83dacc1ede6fdcffe4
onlyHydra <chiperstefantheodor@gmail.com>
2025-05-05 20:38:20 +0200

    Added basic framework for more syntax check and extra waiting behaviour #12

--- sources/tokener/interface/token_interface/token_interface.c
+++ sources/tokener/interface/token_interface/token_interface.c
@@ -6,7 +6,7 @@
 /*   By: schiper <schiper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 02:31:51 by iatilla-          #+#    #+#             */
-/*   Updated: 2025/05/05 20:32:37 by schiper          ###   ########.fr       */
+/*   Updated: 2025/05/05 20:38:13 by schiper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,8 @@ void	add_token(t_token **head, char *value, t_token_type type, char * filepath)
 		current = *head;
 		while (current->next)
 			current = current->next;
-        if(syntax_check(curret,new_token))
-		current->next = new_token;
+        if(syntax_check(current,new_token))
+		    current->next = new_token;
 	}
 }
 
