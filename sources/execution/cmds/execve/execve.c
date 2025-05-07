3d3d9bb9d76c3b95675fee25cf86e217dbb72b4f
Idriz Atilla Martin <iatilla-@c2r8s8.42wolfsburg.de>
2025-05-07 18:44:38 +0200

    [FIX]

--- sources/execution/cmds/execve/execve.c
+++ sources/execution/cmds/execve/execve.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execve.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: schiper <schiper@student.42.fr>            +#+  +:+       +#+        */
+/*   By: iatilla- <iatilla-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 23:06:04 by schiper           #+#    #+#             */
-/*   Updated: 2025/05/07 00:56:34 by schiper          ###   ########.fr       */
+/*   Updated: 2025/05/07 18:34:54 by iatilla-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,16 @@ int	run_execve(char *filepath, char **argv, char **envp)
 	if (filepath == NULL || access(filepath, F_OK) == -1)
 	{
 		error_code = 127;
-		print_execve_error(filepath, error_code);
+		print_execve_error( argv[0], error_code);
 		return (error_code);
 	}
 	if (access(filepath, X_OK) == -1)
 	{
 		error_code = 126;
-		print_execve_error(filepath, error_code);
+		print_execve_error(argv[0], error_code);
 		return (error_code);
 	}
 	if (execve(filepath, argv, envp) == -1)
-	{
 		error_code = 1;
-		print_execve_error(filepath, error_code);
-
-	}
 	return (error_code);
 }
