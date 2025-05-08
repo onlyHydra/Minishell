818858508b3ffe3b86c7b0e8bcaebad951e47061
Idriz Atilla Martin <iatilla-@c2r9s7.42wolfsburg.de>
2025-05-08 14:33:04 +0200

    [NEW]

--- sources/execution/pipe/execute_command.c
+++ sources/execution/pipe/execute_command.c
@@ -6,7 +6,7 @@
 /*   By: iatilla- <iatilla-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 14:38:45 by schiper           #+#    #+#             */
-/*   Updated: 2025/05/08 13:52:53 by iatilla-         ###   ########.fr       */
+/*   Updated: 2025/05/08 14:06:50 by iatilla-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,19 +54,6 @@ static int	pre_check_command(t_cmd *cmd, t_exec_ctx *ctx)
 	return (exit_code);
 }
 
-static void print_exit_status(int exit_status)
-{
-	char *status_str;
-	
-	status_str = ft_itoa(exit_status);
-	if (status_str)
-	{
-		write(STDOUT_FILENO, status_str, ft_strlen(status_str));
-		write(STDOUT_FILENO, "\n", 1);
-		free(status_str);
-	}
-}
-
 int	execute_command(t_node *node, t_exec_ctx *ctx, int pipe_flag)
 {
 	pid_t	pid;
@@ -76,16 +63,6 @@ int	execute_command(t_node *node, t_exec_ctx *ctx, int pipe_flag)
 	pid = -1;
 	status = -2;
 	cmd = node->u_data.cmd;
-	if (cmd->cmd_path && ft_strcmp(cmd->cmd_path, "$?") == 0)
-	{
-		print_exit_status(ctx->exit_status);
-		return (ctx->exit_status);
-	}
-	if (cmd->argv && cmd->argv[0] && ft_strcmp(cmd->argv[0], "$?") == 0)
-	{
-		print_exit_status(ctx->exit_status);
-		return (ctx->exit_status);
-	}
 	if (ft_strcmp(cmd->cmd_path, "built-in") == 0)
 		status = check_unset_export(cmd, ctx);
 	if (status == -2)
--- sources/graph/abstract_syntax_tree/execution/depth_first_search.c
+++ sources/graph/abstract_syntax_tree/execution/depth_first_search.c
@@ -6,7 +6,7 @@
 /*   By: iatilla- <iatilla-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:08:24 by schiper           #+#    #+#             */
-/*   Updated: 2025/05/08 13:40:34 by iatilla-         ###   ########.fr       */
+/*   Updated: 2025/05/08 14:13:47 by iatilla-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@
 int	dfs_walk(t_node *root, t_exec_ctx *ctx, int pipe_flag)
 {
 	int	left_result;
-	int	exit_code;
 
 	if (!root)
 		return (0);
-	exit_code = 0;
 	if (root->type == NODE_COMMAND)
 		return (execute_command(root, ctx, pipe_flag));
 	if (root->type == NODE_AND)
@@ -41,6 +39,5 @@ int	dfs_walk(t_node *root, t_exec_ctx *ctx, int pipe_flag)
 		return (execute_n_pipe(root, ctx));
 	if (root->type == NODE_SUBSHELL)
 		return (execute_subshell(root, ctx, pipe_flag));
-	ctx->exit_status = exit_code;
 	return (-1);
 }
